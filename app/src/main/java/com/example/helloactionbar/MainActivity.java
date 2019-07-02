90bf32f6508eb822b532acc9c57aa0d24e0bbf83
Virginia <virlli_nia@hotmail.com>
2019-07-02 16:58:33 +0200

    Add action to button and show a Custom toaster from androidArsenal

--- app/src/main/java/com/example/helloactionbar/MainActivity.java
+++ app/src/main/java/com/example/helloactionbar/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.helloactionbar;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import es.dmoral.toasty.Toasty;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +23,23 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_nuevo:
+                Log.i("ActionBar", "Nuevo!");
+                Toasty.success(getApplicationContext(), "Success!", Toast.LENGTH_SHORT, true).show();
+                return true;
+            case R.id.action_buscar:
+                Log.i("ActionBar", "Buscar!");;
+                return true;
+            case R.id.action_settings:
+                Log.i("ActionBar", "Settings!");;
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
